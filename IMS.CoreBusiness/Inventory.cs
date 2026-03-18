b0b842ea371b1bcd819ee1b55bf966150efe6bfc
Ehsan Pazhooman <ehspazh@gmail.com>
2026-03-18 17:26:37 +0330

    validating data in add inventory form

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,9 +1,18 @@
-﻿namespace IMS.CoreBusiness;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness;
 
 public class Inventory
 {
     public int InventoryId { get; set; }
+    
+    [Required]
+    [MaxLength(150)]
     public string InventoryName { get; set; } = string.Empty;
+    
+    [Range(0,int.MaxValue,ErrorMessage ="Please enter a number greater or equal to 0.")]
     public int Quantity { get; set; }
+    
+    [Range(0,int.MaxValue,ErrorMessage ="Please enter a number greater or equal to 0.")]
     public double Price { get; set; }
 }
